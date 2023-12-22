3c9755cb502703aae9eac5a532d2e2b46d991049
Chris Lane Jones <chrislanejones@gmail.com>
2023-12-22 04:47:38 -0500

    Three Names Returned with useeffect

--- New/src/App.jsx
+++ New/src/App.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 function App() {
   const [names, setNames] = useState([]);
 
-  fetch("/names.json")
-    .then((response) => response.json())
-    .then((data) => setNames(data));
+  useEffect(() => {
+    fetch("/names.json")
+      .then((response) => response.json())
+      .then((data) => setNames(data));
+  }, []);
 
   return <div>Names: {names.join(", ")}</div>;
 }
