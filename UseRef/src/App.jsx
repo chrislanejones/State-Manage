27d8ae9a12f6b1bf7befebd51f633d6e3afde179
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-02 23:54:35 -0500

    Useref name generator

--- UseRef/src/App.jsx
+++ UseRef/src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { useRef } from "react";
 
 function App() {
   const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const [names, SetNames] = useState([
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+  ]);
+
+  const onAddName = () => {
+    SetNames([
+      ...names,
+      {
+        id: 10,
+        name: inputRef.current.value,
+      },
+    ]);
+    inputRef.current.value = "";
+  };
+
   return (
     <div>
+      <div>
+        {names.map((firstName) => (
+          <div key={firstName.name}>{firstName.name}</div>
+        ))}
+      </div>
       <input type="text" ref={inputRef} />
+      <button onClick={onAddName}>Add Name</button>
     </div>
   );
 }
