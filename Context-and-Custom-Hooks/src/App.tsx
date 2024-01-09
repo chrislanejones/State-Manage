550538695126a7ab10d2e8d34ff343f0af303945
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-09 06:00:34 -0500

    createContext: Context pushed through App.tsx to prevent prop drilling

--- Context-and-Custom-Hooks/src/App.tsx
+++ Context-and-Custom-Hooks/src/App.tsx
@@ -25,13 +25,14 @@ function usePokemon(): {
   return { pokemon };
 }
 
-const ThemeContext = createContext("light");
+const PokemonContext = createContext({
+  pokemon: [] as Pokemon[],
+});
 
-const PokemonList = ({ pokemon }: { pokemon: Pokemon }) => {
-  const theme = useContext(ThemeContext);
+const PokemonList = () => {
+  const { pokemon } = useContext(PokemonContext);
   return (
     <div>
-      <div>theme: {theme}</div>
       {pokemon.map((p) => (
         <div key={p.id}>{p.name}</div>
       ))}
@@ -40,11 +41,10 @@ const PokemonList = ({ pokemon }: { pokemon: Pokemon }) => {
 };
 
 function App() {
-  const { pokemon } = usePokemon();
   return (
-    <ThemeContext.Provider value="light">
-      <PokemonList pokemon={pokemon} />;
-    </ThemeContext.Provider>
+    <PokemonContext.Provider value={usePokemon()}>
+      <PokemonList />;
+    </PokemonContext.Provider>
   );
 }
 
