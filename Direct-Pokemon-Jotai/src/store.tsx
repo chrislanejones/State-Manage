d5b67e1bc351ce51f146332b1c5ade115a7b4446
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-30 23:40:02 -0500

    Direct Pokemon Jotai  - atomsWithQuery Setup

--- Direct-Pokemon-Jotai/src/store.tsx
+++ Direct-Pokemon-Jotai/src/store.tsx
@@ -1,3 +1,6 @@
+import { atom } from "jotai";
+import { atomWithQuery } from "jotai-tanstack-query";
+
 export interface Pokemon {
   id: number;
   name: string;
@@ -9,3 +12,10 @@ export interface Pokemon {
   special_defense: number;
   speed: number;
 }
+
+export const searchAtom = atom("");
+
+export const [allPokemon] = atomsWithQuery<Pokemon[]>(() => ({
+  queryKey: ["pokemon"],
+  queryFn: () => fetch("/pokemon.json").then((res) => res.json()),
+}));
